72659f6c84c89c9c21c7ca77bcbeb244a218b20c
OMAR-Zizo827 <omar.abdos2005@gmail.com>
2025-04-12 19:22:18 +0200

    commit

--- EarlyPriQueue.h
+++ EarlyPriQueue.h
@@ -10,6 +10,31 @@ private:
 public:
     bool reschedule(int& maxPt)
     {
+        if (this->count == 0)
+            return false;
+        int index = rand() % this->count;
+        priNode<T>* prev = nullptr;
+        priNode<T>* ptr = this->head;
+        for (int i = 0; i < index; i++)
+        {
+            prev = ptr;
+            ptr = ptr->getNext();
+        }
+        if (prev)
+            prev->setNext(ptr->getNext());
+        else
+            this->head = ptr->getNext();
+        this->count--;
+        int temp = 0;
+        int oldPt = ptr->getItem(temp)->getPt();
+        if (oldPt == maxPt)
+            maxPt += 10;
+        if (oldPt > maxPt)
+            maxPt = oldPt + 10;
+        int newPt = oldPt + 1 + rand() % maxPt;
+        ptr->getItem(temp)->setPt(newPt);
+
+        this->enqueue(ptr->getItem(temp), -newPt);
         return true;
     }
 };

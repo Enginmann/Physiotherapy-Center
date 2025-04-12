68fff165e9a6afcc331fd3088a9a4dea8a6de542
OMAR-Zizo827 <omar.abdos2005@gmail.com>
2025-04-12 18:43:08 +0200

    reschule

--- EarlyPriQueue.h
+++ EarlyPriQueue.h
@@ -8,66 +8,52 @@ class EarlyPriQueue : public priQueue<T>
 private:
 
 public:
-	bool reschedule(int & maxPt)
-	{
-		if (this->count == 0)
-			return false;
+    bool reschedule(int& maxPt) {
+        if (this->count == 0 || !this->head)
+            return false;
 
-		int index = rand() % this->count;
+        // Select random node
+        int index = rand() % this->count;
+        priNode<T>* prev = nullptr;
+        priNode<T>* curr = this->head;
 
-		if (!this->head)
-			return false;
-		
-		priNode<T>* ptr = this->head;
-		priNode<T>* ptr2 = nullptr;
+        // traverse to the selected node
+        for (int i = 0; i < index; i++) {
+            prev = curr;
+            curr = curr->getNext();
+        }
 
-		if (index == 0)
-		{
-			this->head = this->head->getNext();
-			ptr->setNext(nullptr);
-			int temp = 0;
-			int oldPt = ptr->getItem(temp)->getPt();
-			int newPt = -1;
-			if (oldPt == maxPt)
-				maxPt += 10;
-			while (oldPt >= newPt)
-				newPt = rand() % maxPt;
-			ptr->getItem(temp)->setPt(newPt);
+        // remove the node from the list
+        if (prev)
+            prev->setNext(curr->getNext());
+        else
+            this->head = curr->getNext();
 
-			this->enqueue(ptr->getItem(temp), -newPt);
-		}
-		else if (this->head->getNext())
-		{
-			for (int i = 0; i < index - 1; i++)
-				ptr = ptr->getNext();
+        // decrement count
+        this->count--;
 
-			ptr2 = ptr->getNext();
-			ptr->setNext(ptr2->getNext());
-			ptr2->setNext(nullptr);
-			int temp = 0;
-			int oldPt = ptr2->getItem(temp)->getPt();
-			int newPt = -1;
-			if (oldPt == maxPt)
-				maxPt += 10;
-			while (oldPt >= newPt)
-				newPt = rand() % maxPt;
-			ptr2->getItem(temp)->setPt(newPt);
-		
-			this->enqueue(ptr2->getItem(temp), -newPt);
-		}
-		else /// head only
-		{
-			int temp = 0;
-			int oldPt = this->head->getItem(temp)->getPt();
-			int newPt = -1;
-			if (oldPt == maxPt)
-				maxPt += 10;
-			while (oldPt >= newPt)
-				newPt = rand() % maxPt;
-			this->head->getItem(temp)->setPt(newPt);
-		}
+        // Get the patient and current Pt
+        int temp;
+        T patient = curr->getItem(temp);
+        int oldPt = patient->getPt();
 
-		return true;
-	}
+        // edge cases
+        if (oldPt == maxPt)
+            maxPt += 10;
+        else if (maxPt < oldPt)
+            maxPt = oldPt + 10;
+
+        // must newPt > oldPt
+        int newPt = oldPt + 1 + rand() % (maxPt - oldPt);
+        patient->setPt(newPt);
+
+        // reinsert with newPt
+        this->enqueue(patient, -newPt);  
+
+        // delete curr
+        curr->setNext(nullptr);
+        delete curr;
+        return true;
+    }
 };
 

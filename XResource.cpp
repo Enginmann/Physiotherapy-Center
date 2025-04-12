f57f6295f8e503671cbff156f7104d84034276f8
Enginmann <103683744+Enginmann@users.noreply.github.com>
2025-04-12 19:18:25 +0200

    more colors

--- XResource.cpp
+++ XResource.cpp
@@ -9,7 +9,15 @@ XResource::XResource(int id, char type, int capacity) : Resource(id, type)
 
 void XResource::print(int count)
 {
+	const char * resetColor = "\033[0m";
+	const char * purpleColor = "\033[35m";
+	
+	cout << resetColor;
+	cout << purpleColor;
+
 	cout << "R" << this->id << "[" <<count << "," << capacity << "]";
+
+	cout << resetColor;
 }
 
 int XResource::getCapacity()

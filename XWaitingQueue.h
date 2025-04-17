92711d8bb58d3dbe153aae3150efb298d183561f
Enginmann <103683744+Enginmann@users.noreply.github.com>
2025-04-17 23:27:11 +0200

    commit

--- XWaitingQueue.h
+++ XWaitingQueue.h
@@ -49,56 +49,10 @@ public:
 	} 
 
 	/// Phase 2 Cancel
-	bool cancel2(T & patient)
+	bool cancel_ph2(T& patient)
 	{
 		if (this->count == 0)
 			return false;
-
-		Node<T> * temp = this->frontPtr;
-		bool isPossible = false;
-		while (temp)
-		{
-			if (temp->getItem()->getTreatment())
-			temp = temp->getNext();
-		}
-
-
-		if (this->count == 1)
-		{
-			patient = this->frontPtr->getItem();
-			delete this->frontPtr;
-
-			this->frontPtr = nullptr;
-			this->backPtr = nullptr;
-
-			this->count--;
-			return true;
-		}
-
-		Node<T> * ptr = this->frontPtr;
-		Node<T> * ptr2 = this->frontPtr;
-		int index = rand() % this->count;
-
-		for (int i = 0; i < index - 1; i++)
-			ptr = ptr->getNext();
-
-		ptr2 = ptr->getNext();
-
-		if (index == this->count - 1)
-		{
-			ptr->setNext(nullptr);
-			this->backPtr = ptr;
-			patient = ptr2->getItem();
-			this->count--;
-			return true;
-		}
-		return true;
-	} 
-
-	bool cancel_ph2(T& patient) {// for phase 2.
-		
-			if (this->count == 0)
-				return false;
 
 		if (this->count == 1)
 		{
